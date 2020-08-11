33ea301d96342578bff29d9e980c28858c11e9fa
shubhral <shubhral.kumar@truecaller.com>
2020-08-11 16:14:56 +0530

    code refactored for scoping class

--- lib/src/scope_options.dart
+++ lib/src/scope_options.dart
@@ -1,23 +1,34 @@
-class FlutterTruecallerSdkScope {
+class TruecallerSdkScope {
+  /// footer options
   static const int FOOTER_TYPE_SKIP = 1;
   static const int FOOTER_TYPE_CONTINUE = 2;
-  static const int CONSENT_MODE_POPUP = 4;
-  static const int CONSENT_MODE_FULLSCREEN = 8;
-  static const int SDK_OPTION_WITHOUT_OTP = 16;
-  static const int SDK_OPTION_WITH_OTP = 32;
   static const int FOOTER_TYPE_NONE = 64;
-  static const int CONSENT_MODE_BOTTOMSHEET = 128;
   static const int FOOTER_TYPE_ANOTHER_METHOD = 256;
   static const int FOOTER_TYPE_MANUALLY = 512;
-  static const int BUTTON_SHAPE_ROUNDED = 1024;
-  static const int BUTTON_SHAPE_RECTANGLE = 2048;
   static const int FOOTER_TYPE_LATER = 4096;
+
+  /// consent mode options
+  static const int CONSENT_MODE_POPUP = 4;
+  static const int CONSENT_MODE_FULLSCREEN = 8;
+  static const int CONSENT_MODE_BOTTOMSHEET = 128;
+
+  /// sdk options
+  static const int SDK_OPTION_WITHOUT_OTP = 16;
+  static const int SDK_OPTION_WITH_OTP = 32;
+
+  /// title options (applicable for [CONSENT_MODE_POPUP] and [CONSENT_MODE_FULLSCREEN])
   static const int SDK_CONSENT_TITLE_LOG_IN = 0;
   static const int SDK_CONSENT_TITLE_SIGN_UP = 1;
   static const int SDK_CONSENT_TITLE_SIGN_IN = 2;
   static const int SDK_CONSENT_TITLE_VERIFY = 3;
   static const int SDK_CONSENT_TITLE_REGISTER = 4;
   static const int SDK_CONSENT_TITLE_GET_STARTED = 5;
+
+  /// button shape options (applicable for [CONSENT_MODE_BOTTOMSHEET])
+  static const int BUTTON_SHAPE_ROUNDED = 1024;
+  static const int BUTTON_SHAPE_RECTANGLE = 2048;
+
+  /// login text prefix options (applicable for [CONSENT_MODE_BOTTOMSHEET])
   static const int LOGIN_TEXT_PREFIX_TO_GET_STARTED = 0;
   static const int LOGIN_TEXT_PREFIX_TO_CONTINUE = 1;
   static const int LOGIN_TEXT_PREFIX_TO_PLACE_ORDER = 2;
@@ -34,12 +45,16 @@ class FlutterTruecallerSdkScope {
   static const int LOGIN_TEXT_PREFIX_TO_GET_UPDATES = 13;
   static const int LOGIN_TEXT_PREFIX_TO_SUBSCRIBE = 14;
   static const int LOGIN_TEXT_PREFIX_TO_SUBSCRIBE_AND_GET_UPDATES = 15;
+
+  /// login text suffix options (applicable for [CONSENT_MODE_BOTTOMSHEET])
   static const int LOGIN_TEXT_SUFFIX_PLEASE_VERIFY_MOBILE_NO = 0;
   static const int LOGIN_TEXT_SUFFIX_PLEASE_LOGIN = 1;
   static const int LOGIN_TEXT_SUFFIX_PLEASE_SIGNUP = 2;
   static const int LOGIN_TEXT_SUFFIX_PLEASE_LOGIN_SIGNUP = 3;
   static const int LOGIN_TEXT_SUFFIX_PLEASE_REGISTER = 4;
   static const int LOGIN_TEXT_SUFFIX_PLEASE_SIGN_IN = 5;
+
+  /// button text prefix options (applicable for [CONSENT_MODE_BOTTOMSHEET])
   static const int CTA_TEXT_PREFIX_USE = 0;
   static const int CTA_TEXT_PREFIX_CONTINUE_WITH = 1;
   static const int CTA_TEXT_PREFIX_PROCEED_WITH = 2;
